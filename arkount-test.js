e36233cdbb33e4d908f8026ef43c25f3b8c54548
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-07 11:34:47 +0800

    Updated test.

--- arkount-test.js
+++ arkount-test.js
@@ -4,8 +4,14 @@ const arkount = require( "./arkount.js" );
 assert.equal( arkount( ), 0 );
 assert.equal( arkount( [ ] ), 0 );
 
+assert.equal( arkount( { } ), 0, "should have value 0" );
+assert.equal( arkount( Array ), 0, "should have value 0" );
+assert.equal( arkount( Object ), 0, "should have value 0" );
+
 var array = [ ];
 array[ 1000 ] = 10;
 assert.equal( arkount( array ), 1 );
+
+assert.equal( arkount( [ { "hello": "world", "name": "simple" }, { "option": { "merchant": "biyaheroes" } } ] ), 2, "should have value 2" );
 
 console.log( "ok" );
