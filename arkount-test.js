5e28368355773258a95c6c81c7713cec8cedcd40
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-04 15:37:24 +0800

    Updated server-side test.

--- arkount-test.js
+++ arkount-test.js
@@ -1,17 +1,26 @@
 const assert = require( "assert" );
 const arkount = require( "./arkount.js" );
 
-assert.equal( arkount( ), 0 );
-assert.equal( arkount( [ ] ), 0 );
+assert.equal( arkount( [ 1, 2, 3 ] ), 3, "should return 3" );
 
-assert.equal( arkount( { } ), 0, "should have value 0" );
-assert.equal( arkount( Array ), 0, "should have value 0" );
-assert.equal( arkount( Object ), 0, "should have value 0" );
+assert.equal( arkount( [ [ 1, 2, 3 ], [ 4, 5, 6 ] ] ), 2, "should return 2" );
 
-var array = [ ];
+let array = [ ];
 array[ 1000 ] = 10;
-assert.equal( arkount( array ), 1 );
+assert.equal( arkount( array ), 1, "should return 1" );
 
-assert.equal( arkount( [ { "hello": "world", "name": "simple" }, { "option": { "merchant": "biyaheroes" } } ] ), 2, "should have value 2" );
+assert.equal( arkount( [ { "hello": "world", "name": "simple" },
+				{ "option": { "merchant": "biyaheroes" } } ] ),
+				2, "should return 2" );
+
+let data = { };
+data[ Symbol( "hello" ) ] = 12345;
+assert.equal( arkount( [ data ] ), 1, "should return 1" );
+
+assert.equal( arkount( ), 0, "should return 0" );
+
+assert.equal( arkount( [ ] ), 0, "should return 0" );
+
+assert.equal( arkount( { } ), 0, "should return 0" );
 
 console.log( "ok" );
